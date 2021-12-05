6f19f17678aab1527554e50a0c8d734bb9778c71
Lucas Maniero <lucasfomaniero@gmail.com>
2021-12-05 16:04:55 -0300

    Created TableView

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -7,11 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    private var pictures: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        navigationController?.navigationBar.barStyle = .black
+        let fileManager  = FileManager.default
+        let imagesPath = Bundle.main.resourcePath!
+        let bundleItems = try? fileManager.contentsOfDirectory(atPath: imagesPath)
+        self.navigationItem.title = "Storm Viewer"
+        self.navigationItem.largeTitleDisplayMode = .never
+        guard let items = bundleItems else { return }
+
+        items.forEach { item in
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "picture", for: indexPath)
+        print(indexPath.row)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
     }
 
 
